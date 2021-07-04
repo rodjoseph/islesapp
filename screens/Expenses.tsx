bd37fbf11d07817df6ee86e332557752beff2f36
Rod Joseph <rodjoseph98@gmail.com>
2021-07-04 07:16:47 -0400

    add item modal for expense tracker

--- screens/Expenses.tsx
+++ screens/Expenses.tsx
@@ -1,16 +1,90 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import React from 'react';
-import { ListRenderItem } from 'react-native';
+import { ListRenderItem} from 'react-native';
 import { Alert } from 'react-native';
 import { FlatList, TouchableOpacity } from 'react-native-gesture-handler';
-import { View, Text } from '../components/Themed';
 import {FontAwesome5} from '@expo/vector-icons'
-import {FloatingButton} from 'react-native-ui-lib';
+import {Colors, FloatingButton, ListItem, MaskedInput, Modal, Picker, TextField, View, Text} from 'react-native-ui-lib';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 type TrackerItem = {
     amount: string,
     name: string
     type: "Saving" | "Expense" | "Income"
+}
+
+const TRACKERITEM_TYPE_OPTIONS = [
+  {label: 'Saving', value: 'Saving'},
+  {label: 'Expense', value: 'Expense'},
+  {label: 'Income', value: 'Income'},
+];
+
+interface TrackerItemModalProps {
+  visible: boolean,
+  onClose: () => void,
+  trackerItem?: TrackerItem
+  onSubmit: (trackerItem: TrackerItem) => void
+}
+
+export const TrackerItemModal = (props: TrackerItemModalProps) => {
+  const {
+    visible,
+    onClose,
+    trackerItem,
+    onSubmit
+  } = props;
+
+  const [newTrackerItem, setNewTrackerItem] = React.useState<TrackerItem>()
+  const [errorMsg, setErrorMsg] = React.useState("")
+  return(
+    <Modal visible={visible} onRequestClose={onClose}>
+      <SafeAreaView>
+      <Modal.TopBar
+        title="Tracker Item"
+        onCancel={onClose}
+        onDone={() => {
+          Alert.alert(JSON.stringify(newTrackerItem));
+          onClose()
+        }}
+        doneButtonProps={{color: Colors.orange30}}
+        cancelButtonProps={{iconStyle: {tintColor: Colors.orange30}}}
+        includeStatusBar
+      />
+      <TextField
+        title="Name"
+        editable
+        placeholder="Name"
+      />
+      <TextField
+        title="Amount"
+        editable
+        placeholder="Amount"
+        value={trackerItem?.amount}
+        onChange={(txt: string) => {
+          console.log(txt)
+          setErrorMsg(isNaN(parseFloat(txt as string)) ? "Enter number" : "");
+          setNewTrackerItem({...newTrackerItem!, amount: txt as string})
+        }}
+        enableErrors
+        error={errorMsg}
+        errorColor="red"
+        prefix="$"
+      />
+      <Picker
+            title="Type"
+            placeholder="Pick a Type"
+            useNativePicker
+            value={trackerItem?.type}
+            onChange={(nativePickerValue: string) => setNewTrackerItem({...newTrackerItem!, type: nativePickerValue})}
+            containerStyle={{marginTop: 20}}
+          >
+            {TRACKERITEM_TYPE_OPTIONS.map(option => (
+              <Picker.Item key={option.value} value={option.value} label={option.label}/>
+            ))}
+          </Picker>
+      </SafeAreaView>
+    </Modal>
+  )
 }
 export const Expenses = () => {
 
@@ -227,10 +301,28 @@ export const Expenses = () => {
     
     const _renderItem: ListRenderItem<TrackerItem> = ({item}) => {
         return(
-            <TouchableOpacity style={{marginVertical: 4}}>
-                <Text numberOfLines={1}>{item.name}</Text>
-                <Text>{item.amount}</Text>
-            </TouchableOpacity>
+          <ListItem
+          height={77.5}
+          containerStyle={{paddingHorizontal: 14}}
+          onPress={() => Alert.alert(`pressed on order #${item.name + 1}`)}
+        >
+          {/* <ListItem.Part left>
+            <Animatable.Image
+              source={{uri: row.mediaUrl}}
+              style={styles.image}
+              {...imageAnimationProps}
+            />
+          </ListItem.Part> */}
+          <ListItem.Part middle column containerStyle={{paddingRight: 17}}>
+            <ListItem.Part containerStyle={{marginBottom: 3}}>
+              <Text text70 style={{flex: 1, marginRight: 10}} numberOfLines={1}>{item.name}</Text>
+              <Text dark10 text70 style={{marginTop: 2}}>{item.amount}</Text>
+            </ListItem.Part>
+            <ListItem.Part>
+              <Text text90 numberOfLines={1}>{item.type}</Text>
+            </ListItem.Part>
+          </ListItem.Part>
+        </ListItem>
         )
     }
     // React.useEffect(() => 
@@ -257,20 +349,22 @@ export const Expenses = () => {
         },
       ];
 
+      const [modalVisible, setModalVisible] = React.useState(false)
     return(
         <View>
-            <FlatList data={trackerItems} contentContainerStyle={{paddingHorizontal: 14}} renderItem={_renderItem} showsVerticalScrollIndicator={false}/>       
-            {/* <FloatingAction
-                actions={actions}
-                onPressItem={name => {
-                    switch(name) {
-                        case "expense":
-                        case "savings":
-                        case "income":
-                            console.log(name);
-                    }
-                }}
-            /> */}<FloatingButton/>
+          <FlatList data={trackerItems} renderItem={_renderItem} showsVerticalScrollIndicator={false}/>       
+          <FloatingButton
+          visible
+          button={{
+            label: 'Add Expense',
+            onPress: () => setModalVisible(true)
+          }}
+          hideBackgroundOverlay
+          // bottomMargin={80}
+          // hideBackgroundOverlay
+          // withoutAnimation
+        />
+        <TrackerItemModal visible={modalVisible} onClose={() => setModalVisible(false)} onSubmit={() => {}}/>
         </View>
     )
 }  
